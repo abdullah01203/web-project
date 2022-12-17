28b6f1e13ebf487698ef70d9737e43ada0ac6f0f
abdullah01203 <120250657+abdullah01203@users.noreply.github.com>
2022-12-17 16:00:42 +0200

    Add files via upload

--- New folder (7)/js/script.js
+++ New folder (7)/js/script.js
@@ -1,3 +1,4 @@
+
 const icon = document.getElementById("icon");
 icon.addEventListener("click", () => {
 document.body.classList.toggle("light-theme");
@@ -7,7 +8,7 @@ icon.src ="PNG & SVG/moon.png";
 icon.src = "PNG & SVG/sun.png";
 }
 
-
+alert(window.screen.width+"*"+window.screen.height);
 });
 const icon2 = document.getElementById("icon2");
 icon2.addEventListener("click", () => {
@@ -19,7 +20,7 @@ icon2.src = "PNG & SVG/sun.png";
 }
 
 
-})
+});
 const menu = document.getElementById("menu");
 const vr = document.getElementById("vr");
 menu.addEventListener("click", ()=>{
@@ -30,28 +31,32 @@ menu.classList.toggle("is-active");
 vr.classList.toggle("is-active");
 }
 });
+function page(){
+    document.getElementById("page1").style.display="none";
+    document.getElementById("page2").style.display="block";  
+}
 function fun(){
         
     if (document.getElementById("active").getAttribute('src')=="PNG & SVG/Asset 2.svg"){
-    document.getElementById("mydiv1").style.margin="0 0 0 3.5rem";
+    document.getElementById("mydiv1").style.margin="var(--margin1)";
     document.getElementById("mydiv1").style.opacity="100%";
     document.getElementById("mydiv1").style.width="3.2rem";
-    document.getElementById("mydiv2").style.margin="0 0 0 6.5rem";
+    document.getElementById("mydiv2").style.margin="var(--margin2)";
     document.getElementById("mydiv2").style.opacity="100%";
     document.getElementById("mydiv2").style.width="3.2rem";
-    document.getElementById("mydiv3").style.margin="0 0 0 -1.7rem";
+    document.getElementById("mydiv3").style.margin="var(--margin3)";
     document.getElementById("mydiv3").style.opacity="100%";
     document.getElementById("mydiv3").style.width="3.2rem";
     document.getElementById("mydiv4").style.width="3.2rem";
-    document.getElementById("mydiv4").style.margin="0 0 0 -4.7rem";
+    document.getElementById("mydiv4").style.margin="var(--margin4)";
     document.getElementById("mydiv4").style.opacity="100%";
     document.getElementById("mydiv4").style.width="3.2rem";
     document.getElementById("active").src="PNG & SVG/Artboard 1.svg";
     document.getElementById("active").style.width="1.5rem";
-    document.getElementById("active").style.margin="1.1rem 0 0 1.7rem";
+    document.getElementById("active").style.margin="var(--mrgin)";
     document.getElementById("active2").src="PNG & SVG/Artboard 2.svg";
     document.getElementById("active2").style.width="1.5rem";
-    document.getElementById("active2").style.margin="1.1rem 0 0 1.7rem";
+    document.getElementById("active2").style.margin="var(--mrgin)";
     document.getElementById("mydiv1").style.rotate="360deg";
     document.getElementById("mydiv2").style.rotate="360deg";
     document.getElementById("mydiv3").style.rotate="-360deg";
@@ -74,10 +79,10 @@ else {
     document.getElementById("mydiv4").style.width="2.5rem";
     document.getElementById("active").src="PNG & SVG/Asset 2.svg";
     document.getElementById("active").style.width="3.2rem";
-    document.getElementById("active").style.margin="0 0 0 12px";
+    document.getElementById("active").style.margin="var(--null)";
     document.getElementById("active2").src="PNG & SVG/SVG/Asset 2.svg";
     document.getElementById("active2").style.width="3.2rem";
-    document.getElementById("active2").style.margin="0 0 0 12px";
+    document.getElementById("active2").style.margin="var(--null)";
     document.getElementById("mydiv1").style.rotate="270deg";
     document.getElementById("mydiv2").style.rotate="270deg";
     document.getElementById("mydiv3").style.rotate="-270deg";
@@ -101,17 +106,17 @@ function fun11(m){
     if (document.getElementById("hid1").style.display==0 || document.getElementById("hid1").style.display=="none" ){
     document.getElementById("hid1").style.display="contents";
     document.getElementById("hid11").style.display="contents";
-    document.getElementById("crd1").style.width="250px";
+    document.getElementById("crd1").style.width="var(--width)";
     document.getElementById("crd1").style.margin="0 0";
     
 }
 
 
 else{
-    document.getElementById("crd1").style.margin=" 10rem 0";
+    document.getElementById("crd1").style.margin=" var(--margin0)";
     document.getElementById("hid1").style.display="none";
     document.getElementById("hid11").style.display="none";
-    document.getElementById("crd1").style.width="160px";
+    document.getElementById("crd1").style.width="var(--width2)";
     }
 }
 
@@ -121,16 +126,16 @@ function fun12(m){
     if (document.getElementById("hid2").style.display==0 || document.getElementById("hid2").style.display=="none" ){
     document.getElementById("hid2").style.display="contents";
     document.getElementById("hid12").style.display="contents";
-    document.getElementById("crd2").style.width="250px";
+    document.getElementById("crd2").style.width="var(--width)";
     document.getElementById("crd2").style.margin="0 0";
 
 }
 
 else{
-    document.getElementById("crd2").style.margin=" 10rem 0";
+    document.getElementById("crd2").style.margin=" var(--margin0)";
     document.getElementById("hid2").style.display="none";
     document.getElementById("hid12").style.display="none";
-    document.getElementById("crd2").style.width="160px";
+    document.getElementById("crd2").style.width="var(--width2)";
     }}
 
 
@@ -139,16 +144,16 @@ function fun13(m){
     if (document.getElementById("hid3").style.display==0 || document.getElementById("hid3").style.display=="none" ){
     document.getElementById("hid3").style.display="contents";
     document.getElementById("hid13").style.display="contents";
-    document.getElementById("crd3").style.width="250px";
+    document.getElementById("crd3").style.width="var(--width)";
     document.getElementById("crd3").style.margin="0 0";
 
 }
 
 else{
-    document.getElementById("crd3").style.margin=" 10rem 0";
+    document.getElementById("crd3").style.margin=" var(--margin0)";
     document.getElementById("hid3").style.display="none";
     document.getElementById("hid13").style.display="none";
-    document.getElementById("crd3").style.width="160px";
+    document.getElementById("crd3").style.width="var(--width2)";
     }}
 
 
@@ -157,16 +162,16 @@ function fun14(m){
     if (document.getElementById("hid4").style.display==0 || document.getElementById("hid4").style.display=="none" ){
     document.getElementById("hid4").style.display="contents";
     document.getElementById("hid14").style.display="contents";
-    document.getElementById("crd4").style.width="250px";
+    document.getElementById("crd4").style.width="var(--width)";
     document.getElementById("crd4").style.margin="0 0";
 
 }
 
 else{
-    document.getElementById("crd4").style.margin=" 10rem 0";
+    document.getElementById("crd4").style.margin=" var(--margin0)";
     document.getElementById("hid4").style.display="none";
     document.getElementById("hid14").style.display="none";
-    document.getElementById("crd4").style.width="160px";
+    document.getElementById("crd4").style.width="var(--width2)";
     }}
     function fun4(){
         
@@ -175,47 +180,48 @@ else{
        }
         
         if (document.getElementById("im1").style.width=="0px"||document.getElementById("im1").style.width==0){
-        document.getElementById("im1").style.width="70px";
-        document.getElementById("im1").style.height="70px";
+        document.getElementById("im1").style.width="var(--hi)";
+        document.getElementById("im1").style.height="var(--hi)";
         document.getElementById("m1").style.height="70%";
-        document.getElementById("im2").style.width="70px";
-        document.getElementById("im2").style.height="70px";
+        document.getElementById("im2").style.width="var(--hi)";
+        document.getElementById("im2").style.height="var(--hi)";
         document.getElementById("m2").style.height="70%";
-        document.getElementById("im3").style.width="70px";
-        document.getElementById("im3").style.height="70px";
+        document.getElementById("im3").style.width="var(--hi)";
+        document.getElementById("im3").style.height="var(--hi)";
         document.getElementById("m3").style.height="70%";
-        document.getElementById("im3").style.width="70px";
-        document.getElementById("im3").style.height="70px";
+        document.getElementById("im3").style.width="var(--hi)";
+        document.getElementById("im3").style.height="var(--hi)";
         document.getElementById("m3").style.height="70%";
-        document.getElementById("im4").style.width="70px";
-        document.getElementById("im4").style.height="70px";
+        document.getElementById("im4").style.width="var(--hi)";
+        document.getElementById("im4").style.height="var(--hi)";
         document.getElementById("m4").style.height="70%";
-        document.getElementById("im5").style.width="70px";
-        document.getElementById("im5").style.height="70px";
+        document.getElementById("im5").style.width="var(--hi)";
+        document.getElementById("im5").style.height="var(--hi)";
         document.getElementById("m5").style.height="70%";
-        document.getElementById("im6").style.width="70px";
-        document.getElementById("im6").style.height="70px";
+        document.getElementById("im6").style.width="var(--hi)";
+        document.getElementById("im6").style.height="var(--hi)";
         document.getElementById("m6").style.height="70%";
-        document.getElementById("im7").style.width="70px";
-        document.getElementById("im7").style.height="70px";
+        document.getElementById("im7").style.width="var(--hi)";
+        document.getElementById("im7").style.height="var(--hi)";
         document.getElementById("m7").style.height="70%";
-        document.getElementById("im8").style.width="70px";
-        document.getElementById("im8").style.height="70px";
+        document.getElementById("im8").style.width="var(--hi)";
+        document.getElementById("im8").style.height="var(--hi)";
         document.getElementById("m8").style.height="70%";
-        document.getElementById("im9").style.width="70px";
-        document.getElementById("im9").style.height="70px";
+        document.getElementById("im9").style.width="var(--hi)";
+        document.getElementById("im9").style.height="var(--hi)";
         document.getElementById("m9").style.height="70%";
-        document.getElementById("im10").style.width="70px";
-        document.getElementById("im10").style.height="70px";
+        document.getElementById("im10").style.width="var(--hi)";
+        document.getElementById("im10").style.height="var(--hi)";
         document.getElementById("m10").style.height="70%";
-        document.getElementById("im11").style.width="70px";
-        document.getElementById("im11").style.height="70px";
+        document.getElementById("im11").style.width="var(--hi)";
+        document.getElementById("im11").style.height="var(--hi)";
         document.getElementById("m11").style.height="70%";
-        document.getElementById("im12").style.width="70px";
-        document.getElementById("im12").style.height="70px";
-        document.getElementById("img1").style.height="500px";
+        document.getElementById("im12").style.width="var(--hi)";
+        document.getElementById("im12").style.height="var(--hi)";
+        document.getElementById("img1").style.height="500x";
         document.getElementById("img1").style.width="400px";
         document.getElementById("rel").style.margin="var(--mar)";
+        document.getElementById("rel").style.width="var(--h)";
         document.getElementById("img2").style.width="70%";
         document.getElementById("img2").style.position="relative";
         document.getElementById("im1").style.position="relative";
